292cabd800f67b75843e212615024c78ac794b22
Anna Moshkina <a.v.moshkina@gmail.com>
2019-12-02 13:02:24 +0300

    Day 2: minor cleanup

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,15 +1,14 @@
 use std::fs::read_to_string;
 use std::error::Error;
 
-use num_enum::TryFromPrimitive;
-// use num_enum::Err;
+use num_enum::{TryFromPrimitive, TryFromPrimitiveError};
 
 use std::convert::TryFrom;
 
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[repr(i32)]
+#[repr(usize)]
 #[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
 enum Op {
     Add = 1,
@@ -19,67 +18,67 @@ enum Op {
 
 #[derive(Debug)]
 struct Intcode {
-    code: Vec<i32>,
-    current: usize
+    code: Vec<usize>,
+    ptr: usize
 }
 
 impl Intcode {
+    fn new() -> Self {
+        Self{
+            code: read_to_string("src/input.txt").unwrap().split(',').map(|item| item.parse::<usize>().unwrap()).collect(),
+            ptr: 0
+        }
+    }
+
     fn restore_1202(self: &mut Self) {
         self.code[1] = 12;
         self.code[2] = 2;
     }
 
-    fn get_args(self: &Self) -> (i32, i32) {
-        (self.code[self.code[self.current + 1 as usize] as usize], self.code[self.code[self.current + 2 as usize] as usize])
+    fn get_args(self: &Self) -> (usize, usize) {
+        (self.code[self.code[self.ptr + 1usize]], self.code[self.code[self.ptr + 2usize]])
     }
 
-    fn store_result(self: &mut Self, result: i32) {
-        let index = self.code[self.current + 3 as usize] as usize;
+    fn store_result(self: &mut Self, result: usize) {
+        let index = self.code[self.ptr + 3usize];
         self.code[index] = result
     }
 
     fn finished(self: &Self) -> bool {
-        self.current >= self.code.len()
+        self.ptr >= self.code.len()
     }
 
-    fn op(self: &Self) -> Result<Op, num_enum::TryFromPrimitiveError<Op>> {
-        Op::try_from(self.code[self.current])
+    fn op(self: &Self) -> Result<Op, TryFromPrimitiveError<Op>> {
+        Op::try_from(self.code[self.ptr])
     }
 
     fn next(self: &mut Self) {
-        self.current += 4
+        self.ptr += 4
     }
 }
 
 
 
 fn main() -> MyResult<()> {
-    let mut program: Intcode = Intcode{
-        code: read_to_string("src/input.txt")?.split(',').map(|item| item.parse::<i32>().unwrap()).collect(),
-        current: 0
-    };
-    // program.restore_1202(&mut program);
+    let mut program = Intcode::new();
+    program.restore_1202();
 
-    println!("program: {:?}", program);
-
-    
     while !program.finished() {
-        match program.op() {
-            Ok(Op::Add) => {
+        match program.op()? {
+            Op::Add => {
                 let (arg1, arg2) = program.get_args();
                 program.store_result(arg1 + arg2);
             },
-            Ok(Op::Mult) => {
+            Op::Mult => {
                 let (arg1, arg2) = program.get_args();
                 program.store_result(arg1 * arg2);
             },
-            Ok(Op::Halt) => break,
-            Err(_) => println!("Result Part 1: {:?}", program.code[0])
+            Op::Halt => break,
         }
 
         program.next()
 
     }
-    println!("Finished without errors: {:?}", program);
+    println!("Result Part 1: {:?}", program.code[0]);
     Ok(())
 }
