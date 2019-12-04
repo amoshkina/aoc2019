dbdc385b128c31974a1a85abe0e3446a4c8af462
Anna Moshkina <a.v.moshkina@gmail.com>
2019-12-04 11:46:44 +0300

    Day 4: destroyed part 1, added part 2 instead

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,27 +7,37 @@ fn main() {
     for num in (from..to).map(|int| int.to_string()) {
         let mut chars = num.chars().map(|ch| ch.to_digit(RADIX).unwrap());
 
-        let mut adjacent: bool = false;
         let mut increasing: bool = true;
 
+        let mut adjacent: bool = false;
+        let mut adjacent_counter: u32 = 1;
+        
         let mut prev: u32 = chars.next().unwrap();
         for curr in chars {
             if prev == curr {
-                adjacent = true;
-            }
-
-            if prev > curr {
+                adjacent_counter += 1;
+            } else if prev > curr {
                 increasing = false;
                 break;
+            } else {  // prev < curr
+                if adjacent_counter == 2 {
+                    adjacent = true;
+                }
+                adjacent_counter = 1;
             }
 
             prev = curr;
         }
+
+        if adjacent_counter == 2 {
+            adjacent = true;
+        }
+
 
         if adjacent && increasing {
             counter += 1;
         }
     }
 
-    println!("Result Part 1: {:?}", counter);
+    println!("Result Part 2: {:?}", counter);
 }
