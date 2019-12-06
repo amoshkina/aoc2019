5408b30c9854ec4981eafb092e7dd300b657f245
Anna Moshkina <a.v.moshkina@gmail.com>
2019-12-06 09:15:54 +0300

    Day 6: working draft

---
+++ day6/src/main.rs
@@ -0,0 +1,46 @@
+use serde_scan;
+use serde::Deserialize;
+
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+use std::error::Error;
+use std::collections::{ HashMap};
+
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+
+#[derive(Debug, Deserialize)]
+struct Rel {
+    center: String,
+    satellite: String
+}
+
+fn main() -> MyResult<()> {
+    let file = File::open("src/input.txt")?;
+    let reader = BufReader::new(file);
+    let mut map: HashMap<String, String> = HashMap::new();
+
+    for line in reader.lines() {
+        let line = &line?;
+        let rel: Rel = serde_scan::scan!("{}){}" <- line)?;
+        map.insert(rel.satellite, rel.center);
+    }
+
+    let mut counter: u32 = 0;
+    for (_satellite, center) in &map {
+        // println!("{:?}, {:?}", satellite, center);
+        let mut center: &String = center;
+        counter += 1;
+        while *center != "COM" {
+            // println!("{:?}", center);
+            center = map.get(center).unwrap();//.ok_or("Error");
+            counter += 1;
+        }
+    }
+
+    println!("Result Part 1: {:?}", counter);
+
+    Ok(())
+}
