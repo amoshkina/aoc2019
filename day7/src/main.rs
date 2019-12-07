fc6a57be8d4ddaf53be28293fd4a75c29c2a7723
Anna Moshkina <a.v.moshkina@gmail.com>
2019-12-08 01:11:16 +0300

    Day 7: implemented part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,12 +3,13 @@ use std::error::Error;
 use std::mem::swap;
 use std::cmp::max;
 use std::collections::HashSet;
+use std::ops::Range;
 
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[repr(usize)]
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum Op {
     Add(i32, i32, usize), // 1
     Mult(i32, i32, usize), // 2
@@ -21,7 +22,7 @@ enum Op {
     Halt, // 99
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Intcode {
     code: Vec<i32>,
     iptr: usize,
@@ -106,7 +107,7 @@ impl Intcode {
         self.op = Self::parse_op(&self.code, self.iptr);
     }
 
-    fn run(self: &mut Self, input: &mut Vec<i32>, output: &mut Vec<i32>) -> i32 {
+    fn run(self: &mut Self, input: &mut Vec<i32>, output: &mut Vec<i32>, input_block: bool, output_block: bool) -> i32 {
         while !self.finished() {
             let mut next_addr: Option<usize> = None;
             match self.op {
@@ -117,9 +118,20 @@ impl Intcode {
                 Op::Input(addr) => {
                     let value: i32 = input.pop().unwrap();
                     self.save(value, addr);
+                    if input_block {
+                        self.next(next_addr);
+                        return 3
+                    }
+                    
                 },
 
-                Op::Output(value) => output.push(value),
+                Op::Output(value) => {
+                    output.push(value);
+                    if output_block {
+                        self.next(next_addr);
+                        return 4
+                    }
+                },
 
                 Op::JumpTrue(value, addr) => if value != 0 { next_addr = Some(addr) }, 
 
@@ -139,7 +151,7 @@ impl Intcode {
             }
             self.next(next_addr)
         }
-        self.code[0]
+        0
     }
 }
 
@@ -166,8 +178,7 @@ fn part1(data: &str) -> MyResult<i32> {
 
     let mut result: i32 = 0;
 
-    let mut set: HashSet<i32> = HashSet::new();
-    for item in 0..5 { set.insert(item); }
+    let set = init_set(0..5);
 
     for phases in permutate(set) {
         input = vec![];
@@ -177,9 +188,58 @@ fn part1(data: &str) -> MyResult<i32> {
             input.push(phase);
 
             let mut program = Intcode::new(&data);
-            program.run(&mut input, &mut output);           
+            program.run(&mut input, &mut output, false, false);           
         }
         result = max(result, output.pop().unwrap());
+    }
+    Ok(result)
+}
+
+fn init_set(range: Range<i32>) -> HashSet<i32> {
+    let mut set: HashSet<i32> = HashSet::new();
+    for item in range { set.insert(item); }
+    set
+}
+
+fn feedback_loop(amplifiers: &mut Vec<Intcode>) -> i32 {
+    let (mut input, mut output): (Vec<i32>, Vec<i32>);
+    let mut i: usize = 0;
+    input = vec![0];
+    output = vec![];    
+    loop {
+        assert!(output.is_empty());
+        assert!(!input.is_empty());
+        
+        let ret = amplifiers[i].run(&mut input, &mut output, false, true);
+        swap(&mut input, &mut output);
+        if ret == 0 {
+            break;
+        }
+        
+        i = (i + 1) % 5;
+    }
+    output.pop().unwrap()
+}
+
+
+fn part2(data: &str) -> MyResult<i32> {
+    let (mut input, mut output): (Vec<i32>, Vec<i32>);
+    let mut amplifiers: Vec<Intcode>;
+    let mut result: i32 = 0;
+    let set = init_set(5..10);
+
+
+    for phases in permutate(set) {
+        input = vec![];
+        output = vec![];
+        amplifiers = vec![Intcode::new(&data); 5];
+        // initializing amplifiers with the phase
+        for (i, &phase) in phases.iter().enumerate() {
+            input.push(phase);
+            amplifiers[i].run(&mut input, &mut output, true, true);
+            assert!(input.is_empty());
+        }
+        result = max(result, feedback_loop(&mut amplifiers));
     }
     Ok(result)
 }
@@ -189,6 +249,7 @@ fn main() -> MyResult<()> {
     let data: String = read_to_string("src/input.txt")?;
 
     println!("Result Part 1: {:?}", part1(&data)?);
+    println!("Result Part 2: {:?}", part2(&data)?);
     
     Ok(())
 }
