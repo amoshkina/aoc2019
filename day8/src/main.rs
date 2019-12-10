4eb15cd07ef1bbf4560c280aad4b26fda78bba68
Anna Moshkina <a.v.moshkina@gmail.com>
2019-12-10 21:44:46 +0300

    Day 8: finished part2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,7 +3,9 @@ use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-const LAYER_LEN: usize = 25 * 6;
+const WIDTH: usize = 25;
+const HEIGHT: usize = 6;
+const LAYER_LEN: usize = WIDTH * HEIGHT;
 
 fn count_num(layer: &Vec<i32>, num: i32) -> usize {
     layer.iter().filter(|&&item| item == num).collect::<Vec<&i32>>().len()
@@ -31,10 +33,43 @@ fn part1(data: &str) -> usize {
     result
 }
 
+fn print_image(image: &mut Vec<char>) {
+    println!("image len - {:?}", image.len());
+    let mut from: usize = 0;
+    let mut to = WIDTH;
+
+    while to <= image.len() {
+        let layer: String = image[from..to].iter().map(|&ch| if ch == '0' { "  ".to_string() } else { "0 ".to_string() }).collect();
+        println!("{:?}", layer);
+        from = to;
+        to = from + WIDTH;
+    }
+}
+
+
+fn part2(data: &str) {
+    let data: Vec<char> = data.chars().collect();
+    println!("data - {:?}, layer - {:?}", data.len(), LAYER_LEN);
+    let mut image: Vec<char> = vec![];
+    let mut start: usize = 0;
+    while start < LAYER_LEN {
+        let mut current: usize = start;
+        while data[current] == '2' && current < data.len() {
+            current += LAYER_LEN;
+        }
+        image.push(data[current]);
+        start += 1;
+    }
+
+    print_image(&mut image)
+    
+}
 
 fn main() -> MyResult<()> {
     let data: &str = &read_to_string("src/input.txt")?;
 
     println!("Result Part 1: {:?}", part1(data));
+    println!("Result Part 2: ");
+    part2(data);
     Ok(())
 }
